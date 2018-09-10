15e9faf69730c84b4a08259f0d69a72655e79b66
pvdk84 <pm.vanderkolk@gmail.com>
2018-09-10 19:25:44 +0200

    initial commit

---
+++ TextEditor.java
@@ -0,0 +1,34 @@
+package texteditor;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox; 
+import javafx.stage.Stage;
+
+// nice to have's
+    // tab = python/java compatible
+    // control d = duplicate
+    // copy, cut, paste
+
+public class TextEditor extends Application {
+	
+	@Override
+	public void start(Stage primaryStage) throws Exception {
+		
+		TextArea textArea = new TextArea();
+		
+		VBox vBox = new VBox(textArea);
+		
+		Scene scene = new Scene(vBox, 200, 100);
+		
+		primaryStage.setTitle("My TextEditor");
+		primaryStage.setScene(scene);
+		primaryStage.show();
+	}
+	
+	public static void main(String args[]) {
+		Application.launch(args);
+	}
+}
+
