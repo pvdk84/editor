da4217d78626c62052490194941f0494f81b24f6
pvdk84 <pm.vanderkolk@gmail.com>
2018-10-12 13:31:55 +0200

    added menubaritems and textarea

--- Editor.java
+++ Editor.java
@@ -1,28 +1,47 @@
-package editor;
+package pvdk84.editor;
 
+import pvdk84.editor.EditorMenuBar;
+import pvdk84.editor.EditorTextArea;
 import javafx.application.Application;
-import javafx.scene.Scene;
-import javafx.scene.control.TextArea;
+import javafx.geometry.Insets;
 import javafx.scene.control.MenuBar;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
+ 
 public class Editor extends Application {
+
+	private static Stage stage;
+	private int width = 600;
+	private int height = 400;
+
+ 	public static Stage getStage() {
+        	return stage;
+    	}
 	
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
-		TextArea textArea = new TextArea();
-		
-		MenuBar menuBar = new MenuBar();
-		
-		Scene scene = new Scene(textArea, 800, 600);
-		
-		primaryStage.setTitle("Editor");
-		primaryStage.setScene(scene);
-		primaryStage.show();
-	}
+	public void start(Stage stage) {
+
+		// create vbox root for menubar and textarea
+		VBox root = new VBox();
+       		root.setPadding(new Insets(10));
+        	root.setSpacing(5);
+
+		// add menubar and textarea to root
+		MenuBar menuBar = EditorMenuBar.createMenuBar();
+		TextArea textArea = EditorTextArea.createTextArea(width, height);
+		root.getChildren().addAll(menuBar, textArea);
+
+		// create the whole thing
+ 		Scene scene = new Scene(root, width, height);
+        	stage.setTitle("Simple TextEditor Yo");
+        	stage.setScene(scene);
+        	stage.show();
+    	}
 	
-	public static void main(String args[]) {
-		Application.launch(args);
-	}
+ 	public static void main(String[] args) {
+        	launch(args);
+    	}
 }
--- README.md
+++ README.md
@@ -1,33 +1,24 @@
-=== GENERAL ===
+Just a simple editor to get acquainted with javaFX.
 
-My own custom-made programming editor.
+Features:
+- file menu: open, save, new
+- edit menu: cut, copy, paste
 
-I want it to be as basic and simple as possible, not the fancy stuff of auto-completion and highlighting and so on. Just text, plain and simple. 
+Maybe turn it into my own customized coding editor sometime: 
+- tabs for different open files
+- line numbering
+- find function
+- duplicate, delete line
+- move line up, down
+- comment out
 
-It should look very plain and simple, much like Mac's TextEdit, but with a Matrix/Darcula kind of style: green text against black background.
+Sources:
+- http://www.ntu.edu.sg/home/ehchua/programming/java/Javafx1_intro.html
+- https://github.com/abrandell/JavaFX-TextEditor/tree/master/src/abrand
+- https://github.com/jguitana/simple-file-editor
+- http://www.drdobbs.com/jvm/a-javafx-text-editor-part-1/240142297
+- http://tutorials.jenkov.com/javafx/index.html
+- https://o7planning.org/en/11009/javafx
 
-=== FEATURES ===
-
-"file" menu: open, save, new (keyboard shortcuts)
-
-"edit" menu: cut, copy, paste (keyboard shortcuts)
-
-tabs for different open files
-
-line numbering
-
-key-bindings: (see notepad++)
-- cut, copy, paste: 
-- find: ctrl f
-- duplicate line: ctrl d
-- delete line: ctrl y
-- move line up:  
-- move line down:
-
-=== EXAMPLES ===
-
-http://www.ntu.edu.sg/home/ehchua/programming/java/Javafx1_intro.html
-
-https://github.com/abrandell/JavaFX-TextEditor/tree/master/src/abrand
 
 
